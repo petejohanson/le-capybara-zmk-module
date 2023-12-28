6b4684a79061eeb6f4b51913c46b79dd5d7d2a2c
Peter Johanson <peter@peterjohanson.com>
2023-12-27 22:11:45 -0800

    Add initial loading of settings from memory.

--- drivers/kscan/ec_matrix_settings.c
+++ drivers/kscan/ec_matrix_settings.c
@@ -7,22 +7,25 @@
 #include <string.h>
 #include "zmk_kscan_ec_matrix.h"
 
+
+#define LOG_LEVEL CONFIG_KSCAN_LOG_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(zmk_kscan_ec_matrix_settings);
+
+#define MAX_SETTING_LEN 32
+
 struct load_state {
-    char setting_name[32];
+    char setting_name[MAX_SETTING_LEN];
     struct zmk_kscan_ec_matrix_calibration_entry *entries;
     size_t len;
 };
 
 static int settings_load_cb(const char *key, size_t len, settings_read_cb read_cb, void *cb_arg, void *param) {
     struct load_state *state = (struct load_state *)param;
-
-    if (strcmp(key, state->setting_name) != 0) {
-        return 0;
-    }
     
     ssize_t ret = read_cb(cb_arg, state->entries, state->len * sizeof(struct zmk_kscan_ec_matrix_calibration_entry));
     if (ret < 0) {
-        // OH NO!
+        LOG_ERR("Failed to load the settings from flash");
     }
 
     return ret;
@@ -30,26 +33,39 @@ static int settings_load_cb(const char *key, size_t len, settings_read_cb read_c
 
 static void load_cb(const struct device *dev, struct zmk_kscan_ec_matrix_calibration_entry *entries, size_t len, const void *user_data) {
     struct load_state state = (struct load_state) { .entries = entries, .len = len };
-	sprintf(state.setting_name, "zmk/ec/calibration/%s", dev->name);
+	snprintf(state.setting_name, MAX_SETTING_LEN, "zmk/ec/calibration/%s", dev->name);
+    LOG_DBG("Loading the subtree directly for %s", state.setting_name);
     settings_load_subtree_direct(state.setting_name, settings_load_cb, &state);
 }
 
 static void save_cb(const struct device *dev, struct zmk_kscan_ec_matrix_calibration_entry *entries, size_t len, const void *user_data) {
-	// const struct shell *sh = (const struct shell*)user_data;
-
-	// shell_print(sh, "SAVE %d entries", len);
-
-	char setting_name[32];
-	sprintf(setting_name, "zmk/ec/calibration/%s", dev->name);
+	char setting_name[MAX_SETTING_LEN];
+	snprintf(setting_name, MAX_SETTING_LEN, "zmk/ec/calibration/%s", dev->name);
+    LOG_DBG("Saving the value for %s", setting_name);
 	settings_save_one(setting_name, entries, len * sizeof(struct zmk_kscan_ec_matrix_calibration_entry));
 }
 
 int zmk_kscan_ec_matrix_settings_load_calibration(const struct device *dev) {
     int ret = zmk_kscan_ec_matrix_access_calibration(dev, &load_cb, NULL);
-    return 0;
+    return ret;
 }
 
 int zmk_kscan_ec_matrix_settings_save_calibration(const struct device *dev) {
     int ret = zmk_kscan_ec_matrix_access_calibration(dev, &save_cb, NULL);
     return ret;
 }
+
+#if IS_ENABLED(CONFIG_ZMK_KSCAN_EC_MATRIX_SETTINGS_INIT_LOAD)
+#define DT_DRV_COMPAT zmk_kscan_ec_matrix
+
+#define LOAD_CALL(n) zmk_kscan_ec_matrix_settings_load_calibration(DEVICE_DT_GET(DT_DRV_INST(n)));
+
+static int zmk_kscan_ec_matrix_settings_init(const struct device *dev) {
+    DT_INST_FOREACH_STATUS_OKAY(LOAD_CALL)
+
+	return 0;
+}
+
+SYS_INIT(zmk_kscan_ec_matrix_settings_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+
+#endif // IS_ENABLED(CONFIG_ZMK_KSCAN_EC_MATRIX_SETTINGS_INIT_LOAD)
