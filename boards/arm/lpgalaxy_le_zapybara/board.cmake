c8f38811975afb3dbaacd4019ac1c88aef8d76d8
Peter Johanson <peter@peterjohanson.com>
2024-04-18 23:53:14 -0700

    Add jlink runner for debugging the Le Zapybara PCB.

--- boards/arm/lpgalaxy_le_zapybara/board.cmake
+++ boards/arm/lpgalaxy_le_zapybara/board.cmake
@@ -1,3 +1,5 @@
 # SPDX-License-Identifier: MIT
 
+board_runner_args(jlink "--device=nRF52840_xxAA" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
