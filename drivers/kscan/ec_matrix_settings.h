6663fcc6b590fbc691437ca48cd6d5320db596d4
Peter Johanson <peter@peterjohanson.com>
2024-08-25 23:41:05 -0600

    refactor: Move to ec-support-zmk-module.

--- drivers/kscan/ec_matrix_settings.h
+++
@@ -1,7 +0,0 @@
-
-#include <zephyr/device.h>
-
-#include "zmk_kscan_ec_matrix.h"
-
-int zmk_kscan_ec_matrix_settings_load_calibration(const struct device *dev);
-int zmk_kscan_ec_matrix_settings_save_calibration(const struct device *dev);
